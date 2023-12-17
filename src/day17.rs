8d13dcc1f9784d5f94bfbbdd1a51b759f425c7eb
Artur Zabeyvorota <artur.zabeyvorota@ruter.no>
2023-12-17 19:57:36 +0100

    day 17 temp

--- src/day17.rs
+++ src/day17.rs
@@ -1,4 +1,10 @@
 use std::collections::HashMap;
+use priority_queue::PriorityQueue;
+
+use pathfinding::{
+    directed::dijkstra::dijkstra,
+    matrix::{directions, Matrix},
+};
 
 const UP: u8 = 0;
 const RIGHT: u8 = 1;
@@ -7,8 +13,8 @@ const LEFT: u8 = 3;
 
 pub(crate) fn day17() {
     println!("Day 17");
-    // let file_content = include_str!("../resources/test-input.txt");
-    let file_content = include_str!("../resources/day17.txt").trim();
+    let file_content = include_str!("../resources/test-input.txt");
+    // let file_content = include_str!("../resources/day17.txt").trim();
     // println!("File content:\n{}", file_content);
 
     let map: Vec<_> = file_content.lines().map(|line| {
@@ -16,53 +22,61 @@ pub(crate) fn day17() {
     }).collect();
     // println!("Map: {:?}", map);
 
+    __dijkstra(&map);
+    _dijkstra(&map);
+}
+
+fn __dijkstra(map: &Vec<Vec<usize>>) {
     let start: (usize, usize) = (0, 0);
-    let mut heat_cache: HashMap<(usize, usize, u8, u8), usize> = HashMap::new();
+    let end: (usize, usize) = (map.len()-1, map[0].len()-1);
+    let start_element = Element { position: start, direction: RIGHT, counter: 0 };
+    let path = dijkstra(
+        &start_element,
+        |element| {
+            let moves: Vec<(_, usize)> = possible_moves(&element, &map).iter().map(|e| {
+                let heat = map[e.position.0][e.position.1];
+                (e.clone(), heat)
+            }).collect();
+            moves
+        },
+        |element| element.position == end
+    );
+    println!("Least heat loss for end element: {:?}", path.unwrap().1)
+}
+
+fn _dijkstra(map: &Vec<Vec<usize>>) {
+    let start: (usize, usize) = (0, 0);
+    let end: (usize, usize) = (map.len()-1, map[0].len()-1);
+
     let start_element = Element { position: start, direction: RIGHT, counter: 0 };
 
-    let mut elements_to_discover: Vec<(Element, Vec<(usize, usize)>)> = vec![(start_element, vec![(0, 0)])];
-    while elements_to_discover.len() > 0 {
-        let (element, visited) = elements_to_discover.pop().unwrap();
-        let current_heat = visited.iter().filter(|p| *p != &(0usize, 0usize)).map(|p| map[p.0][p.1]).sum::<usize>();
+    let mut distances: HashMap<Element, usize> = HashMap::new();
+    distances.insert(start_element.clone(), 0);
+
+    let mut elements_to_discover: PriorityQueue<Element, usize> = PriorityQueue::new();
+    elements_to_discover.push((start_element), priority(0));
+
+    while elements_to_discover.peek().unwrap().0.position != end {
+        let (element, _) = elements_to_discover.pop().unwrap();
+        let current_element_heat = *distances.get(&element).unwrap();
         let moves: Vec<_> = possible_moves(&element, &map);
-        // println!("Checking element: {:?}, possible moves: {:?}", element, moves);
         for m in moves {
-            let heat = map[m.position.0][m.position.1];
-            let accumulated_heat = current_heat + heat;
-            let cache_key = (m.position.0, m.position.1, m.direction, m.counter);
-            if heat_cache.contains_key(&cache_key) && *heat_cache.get(&cache_key).unwrap() < accumulated_heat {
-                continue
+            let move_heat = map[m.position.0][m.position.1];
+            if !distances.contains_key(&m) || distances[&m] > current_element_heat + move_heat {
+                distances.insert(m.clone(), current_element_heat + move_heat);
             }
-            if m.position == (map.len()-1, map[0].len()-1) {
-                println!("Reached target, current min={}, cache min {:?}", accumulated_heat, find_end_min(&map, &heat_cache).iter().min());
-            }
-            if !visited.contains(&m.position) {
-                if !heat_cache.contains_key(&cache_key) || *heat_cache.get(&cache_key).unwrap() > accumulated_heat {
-                    heat_cache.insert(cache_key, accumulated_heat);
-                }
-                let mut cloned_visited = visited.clone();
-                cloned_visited.push(m.position);
-                elements_to_discover.push((m, cloned_visited));
-            }
+            elements_to_discover.push(m, priority(current_element_heat + move_heat));
         }
     }
 
-    // recursive_dfs(&start_element, 0, &vec![(0, 0)], &map, &mut heat_cache);
-    println!("Minimal heat loss: {:?}", find_end_min(&map, &heat_cache).iter().min());
+
 }
 
-fn find_end_min(map: &Vec<Vec<usize>>, heat_cache: &HashMap<(usize, usize, u8, u8), usize>) -> Vec<usize> {
-    let results: Vec<_> = [RIGHT, LEFT, UP, DOWN].iter().flat_map(|direction| {
-        let r: Vec<_> = (1..=3).map(|counter| (*direction, counter)).collect();
-        r
-    }).map(|(direction, counter)| {
-        let cache_key = (map.len() - 1, map[0].len() - 1, direction, counter);
-        heat_cache.get(&cache_key)
-    }).filter(|e| e.is_some()).map(|e| *e.unwrap()).collect();
-    results
+fn priority(heat: usize) -> usize {
+    usize::MAX - heat
 }
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
 struct Element { position: (usize, usize), direction: u8, counter: u8 }
 
 fn possible_moves(el: &Element, map: &Vec<Vec<usize>>) -> Vec<Element> {
@@ -98,7 +112,3 @@ fn possible_moves(el: &Element, map: &Vec<Vec<usize>>) -> Vec<Element> {
     }
     result
 }
-
-// min=3991, high
-// min=1474, high
-// min=1406, high
