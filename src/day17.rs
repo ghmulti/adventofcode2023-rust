9a714eefe7dbccee0bc64aca20c9e0bb58371f94
Artur Zabeyvorota <artur.zabeyvorota@ruter.no>
2023-12-17 11:57:46 +0100

    day 17 temp

--- src/day17.rs
+++ src/day17.rs
@@ -7,8 +7,8 @@ const LEFT: u8 = 3;
 
 pub(crate) fn day17() {
     println!("Day 17");
-    let file_content = include_str!("../resources/test-input.txt");
-    // let file_content = include_str!("../resources/day17.txt").trim();
+    // let file_content = include_str!("../resources/test-input.txt");
+    let file_content = include_str!("../resources/day17.txt").trim();
     // println!("File content:\n{}", file_content);
 
     let map: Vec<_> = file_content.lines().map(|line| {
@@ -17,7 +17,7 @@ pub(crate) fn day17() {
     // println!("Map: {:?}", map);
 
     let start: (usize, usize) = (0, 0);
-    let mut heat_cache: HashMap<(usize,usize), usize> = HashMap::new();
+    let mut heat_cache: HashMap<(usize, usize, u8, u8), usize> = HashMap::new();
     let start_element = Element { position: start, direction: RIGHT, counter: 0 };
 
     let mut elements_to_discover: Vec<(Element, Vec<(usize, usize)>)> = vec![(start_element, vec![(0, 0)])];
@@ -29,12 +29,16 @@ pub(crate) fn day17() {
         for m in moves {
             let heat = map[m.position.0][m.position.1];
             let accumulated_heat = current_heat + heat;
-            if heat_cache.contains_key(&m.position) && *heat_cache.get(&m.position).unwrap() < accumulated_heat {
+            let cache_key = (m.position.0, m.position.1, m.direction, m.counter);
+            if heat_cache.contains_key(&cache_key) && *heat_cache.get(&cache_key).unwrap() < accumulated_heat {
                 continue
             }
+            if m.position == (map.len()-1, map[0].len()-1) {
+                println!("Reached target, current min={}, cache min {:?}", accumulated_heat, find_end_min(&map, &heat_cache).iter().min());
+            }
             if !visited.contains(&m.position) {
-                if !heat_cache.contains_key(&m.position) || *heat_cache.get(&m.position).unwrap() >= accumulated_heat {
-                    heat_cache.insert(m.position, accumulated_heat);
+                if !heat_cache.contains_key(&cache_key) || *heat_cache.get(&cache_key).unwrap() > accumulated_heat {
+                    heat_cache.insert(cache_key, accumulated_heat);
                 }
                 let mut cloned_visited = visited.clone();
                 cloned_visited.push(m.position);
@@ -44,32 +48,18 @@ pub(crate) fn day17() {
     }
 
     // recursive_dfs(&start_element, 0, &vec![(0, 0)], &map, &mut heat_cache);
-
-    let end = (map.len()-1, map[0].len()-1);
-    println!("Minimal heat loss: {}", heat_cache.get(&end).unwrap());
+    println!("Minimal heat loss: {:?}", find_end_min(&map, &heat_cache).iter().min());
 }
 
-fn recursive_dfs(e: &Element, accumulated_heat: usize, visited: &Vec<(usize, usize)>, map: &Vec<Vec<usize>>, heat_cache: &mut HashMap<(usize, usize), usize>) {
-    let end = (map.len()-1, map[0].len()-1);
-    let current_heat = if e.position == (0, 0) { 0 } else { map[e.position.0][e.position.1] };
-    let total_heat = accumulated_heat+current_heat;
-    if !heat_cache.contains_key(&e.position) || *heat_cache.get(&e.position).unwrap() > total_heat {
-        heat_cache.insert(e.position, total_heat);
-    }
-    if e.position == end {
-        println!("Reached target position! Current minimum: {}, Cache size: {}", heat_cache.get(&e.position).unwrap(), heat_cache.len());
-        return;
-    }
-    let moves = possible_moves(&e, &map);
-    let filtered_moves: Vec<_> = moves.iter().filter(|e| !visited.contains(&e.position)).collect();
-    for m in filtered_moves {
-        if heat_cache.contains_key(&m.position) && *heat_cache.get(&m.position).unwrap() < total_heat  {
-            continue
-        }
-        let mut cloned_visited = visited.clone();
-        cloned_visited.push(m.position);
-        recursive_dfs(m, accumulated_heat+current_heat, &cloned_visited, map, heat_cache);
-    }
+fn find_end_min(map: &Vec<Vec<usize>>, heat_cache: &HashMap<(usize, usize, u8, u8), usize>) -> Vec<usize> {
+    let results: Vec<_> = [RIGHT, LEFT, UP, DOWN].iter().flat_map(|direction| {
+        let r: Vec<_> = (1..=3).map(|counter| (*direction, counter)).collect();
+        r
+    }).map(|(direction, counter)| {
+        let cache_key = (map.len() - 1, map[0].len() - 1, direction, counter);
+        heat_cache.get(&cache_key)
+    }).filter(|e| e.is_some()).map(|e| *e.unwrap()).collect();
+    results
 }
 
 #[derive(Debug)]
@@ -78,6 +68,13 @@ struct Element { position: (usize, usize), direction: u8, counter: u8 }
 fn possible_moves(el: &Element, map: &Vec<Vec<usize>>) -> Vec<Element> {
     let (row, column) = el.position;
     let mut result: Vec<Element> = vec![];
+    // left
+    if column > 0 && el.direction != RIGHT {
+        let counter = if el.direction == LEFT { el.counter+1 } else { 1 };
+        if counter <= 3 {
+            result.push(Element { position: (row, column - 1), direction: LEFT, counter });
+        }
+    }
     // up
     if row > 0 && el.direction != DOWN  {
         let counter = if el.direction == UP { el.counter+1 } else { 1 };
@@ -99,12 +96,9 @@ fn possible_moves(el: &Element, map: &Vec<Vec<usize>>) -> Vec<Element> {
             result.push(Element { position: (row, column + 1), direction: RIGHT, counter });
         }
     }
-    // left
-    if column > 0 && el.direction != RIGHT {
-        let counter = if el.direction == LEFT { el.counter+1 } else { 1 };
-        if counter <= 3 {
-            result.push(Element { position: (row, column - 1), direction: LEFT, counter });
-        }
-    }
     result
 }
+
+// min=3991, high
+// min=1474, high
+// min=1406, high
